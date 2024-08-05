312527a7e27c710e44d258982707e751f4f1d609
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-08-05 13:33:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-[100px] font-bold">Welcome to My App</h1>
+      </header>
+      <main className="p-4">
+        <p className="text-lg">This is a Tailwind CSS example.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
