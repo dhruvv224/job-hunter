7ca5547db41f1c341f77cdb13e213d9aacb183d1
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-08-06 19:25:09 +0530

    create company

--- Server/Routes/CompanyRoute.js
+++ Server/Routes/CompanyRoute.js
@@ -14,3 +14,24 @@ router.get('/CompanyDetails',async(req,res)=>{
         
     }
 })
+router.post('/PostCompany',async(req,res)=>{
+    try {
+        const{name,location,website,logo}=req.body;
+        const CompanyName=await Company.findOne({name})
+        if(CompanyName)
+        {
+            res.status(400).json({message:'Company already registered try something diffrent name'})
+        }
+        const NewCompany=new Company({
+            name,
+            location,
+            website,
+            logo
+        })
+        await NewCompany.save()
+        
+    } catch (error) {
+        
+    }
+})
+module.exports=router
