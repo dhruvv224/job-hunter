3f6dea03c5863d127ac2d20b839fec0a055beafc
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-08-08 20:22:28 +0530

    commit

--- Server/Server.js
+++ Server/Server.js
@@ -4,6 +4,7 @@ const ConnectDb=require('./Config/Database.js')
 const cors=require('cors')
 ConnectDb()
 app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const PORT=8000;
@@ -18,4 +19,4 @@ app.listen(PORT,()=>{
 // api end points
 app.use('/api/auth/user',require('./Routes/UserRoute.js'))
 app.use('/api/company',require('./Routes/CompanyRoute.js'));
-app.use('/api/jobs/',require('./Routes/JobRoute.js'));
+app.use('/api/jobs/',require('./Routes/JobRoute.js'))
