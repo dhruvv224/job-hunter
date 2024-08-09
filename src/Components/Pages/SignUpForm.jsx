c45ce9799c1a5d1122a55c29e25bf3b57f99e166
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-08-09 20:00:07 +0530

    done

--- src/Components/Pages/SignUpForm.jsx
+++ src/Components/Pages/SignUpForm.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const SignUpForm = () => {
   return (
-    <div className='flex items-center justify-center'></div>
+    <div className='flex items-center justify-center max-w-7xl mx-auto'>
+        <form action='' className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+            <h1 className='font-semibold text-xl'>Sign Up</h1>
+            <div className='my-2'>
+                Full Name
+                <input type='text' className='input' placeholder='Enter your full name'/>
+
+            </div>
+        </form>
+    </div>
   )
 }
 
