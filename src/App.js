1e5c9f3fbddd65d861fd8e5261cbf0613fe3184d
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-08-09 18:29:03 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="bg-blue-500 text-white p-4">
-        <h1 className="text-[100px] font-bold">Welcome to My App</h1>
-      </header>
-      <main className="p-4">
-        <p className="text-lg">This is a Tailwind CSS example.</p>
-      </main>
+    <div className="app">
+      <Navbar/>
+     
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-white'>
+      <div className='flex justify-between mx-auto max-w-7xl h-16'>
+        <div>
+          <h1 className='font-bold text-3xl'>Job<span className='text-red-600 font-sbold text-3xl'>Hunter</span></h1>
+        </div>
+
+      </div>
+      
+    </div>
   )
 }
 
