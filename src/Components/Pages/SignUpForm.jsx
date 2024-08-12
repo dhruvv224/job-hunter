75da4b014ae184d07184129b8bc237b94019c4c3
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-08-12 16:22:18 +0530

    signup

--- src/Components/Pages/SignUpForm.jsx
+++ src/Components/Pages/SignUpForm.jsx
@@ -1,53 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { IoIosEye,IoIosEyeOff } from "react-icons/io";
 
 const SignUpForm = () => {
+  const [password,setPassword]=useState(false)
+  const togglePassword=()=>{
+    setPassword(!password)
+  }
+  const [confirmPass,setConfirmpass]=useState(false)
+  const togglePassword2=()=>{
+    setConfirmpass(!confirmPass)
+  }
   return (
     <div className='flex items-center justify-center max-w-7xl mx-auto'>
-        <form action='' className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
-            <h1 className='font-semibold text-xl'>Sign Up</h1>
-            <div className='my-2'>
-            Full Name
+      <form action='' className='w-1/2 border border-gray-200 rounded-md p-6 my-10'>
+        <h1 className='font-semibold text-2xl mb-4'>Sign Up</h1>
+        
+        <div className='my-4'>
+          <label className='block mb-2 font-medium'>Full Name</label>
+          <input 
+            type='text' 
+            className='border border-gray-300 rounded-md w-full p-2 focus:outline-none focus:border-red-400 duration-150 text-gray-700' 
+            placeholder='Enter full name'
+          />
+        </div>
+        
+        <div className='my-4'>
+          <label className='block mb-2 font-medium'>Email</label>
+          <input 
+            type='email' 
+            className='border border-gray-300 rounded-md w-full p-2 focus:outline-none focus:border-red-400 duration-150 text-gray-700' 
+            placeholder='Enter email'
+          />
+        </div>
+        
+        <div className='my-4'>
+          <label className='block mb-2 font-medium'>Phone Number</label>
+          <input 
+            type='tel' 
+            className='border border-gray-300 rounded-md w-full p-2 focus:outline-none focus:border-red-400 duration-150 text-gray-700' 
+            placeholder='Enter phone number'
+          />
+        </div>
+        
+        <div className='my-4 relative'>
+          <label className='block mb-2 font-medium'>Enter Password</label>
+          <input 
+            type={password ?'text':'password'} 
+            className='border border-gray-300 rounded-md w-full p-2 focus:outline-none focus:border-red-400 duration-150 text-gray-700' 
+            placeholder='Enter password'
+          />
+           <button onClick={togglePassword}      type='button'       className='absolute right-2 top-1/2 transform  bottom-2 text-gray-700 cursor-pointer'
+          >         {
+            password ? <IoIosEyeOff size={20}/>
+            :<IoIosEye size={20}/>
+          }
+          </button>
+        </div>
+        
+        <div className='my-4 relative'>
+          <label className='block mb-2 font-medium'>Re-enter Password</label>
+          <input 
+            type={confirmPass ?'text':'password'}  
+            className='border border-gray-300 rounded-md w-full p-2 focus:outline-none focus:border-red-400 duration-150 text-gray-700' 
+            placeholder='Re-enter password'
+          />
+          <button      onClick={togglePassword2}       className='absolute right-2 top-1/2 transform  bottom-2 text-gray-700 cursor-pointer' type='button'
+          >           {
+            confirmPass ? <IoIosEyeOff size={20}/>
+            :<IoIosEye size={20}/>
+          }
+          </button>
 
-            <div>
+        </div>
+        
+        <div className='my-4'>
+          <label className='block font-medium mb-2'>Role</label>
+          <div className='flex items-center'>
+            <input type='radio' id='applicant' name='role' className='mr-2' />
+            <label htmlFor='applicant' className='mr-6'>Applicant</label>
 
-              <input type='text' className='border border-gray-200 rounded-md ml-2 w-[400px] p-1 focus:border  focus:border-red-400 duration-150 text-gray-500'/> 
-
-            </div>     
-            Email
-
-<div>
-
-  <input type='text' className='border border-gray-200 rounded-md ml-2 w-[400px] p-1 focus:border  focus:border-red-400 duration-150 text-gray-500' placeholder='Enter email'/> 
-  
-</div>  
-Phone number
-
-<div>
-
-  <input type='text' className='border border-gray-200 rounded-md ml-2 w-[400px] p-1 focus:border  focus:border-red-400 duration-150 text-gray-500' placeholder='Enter phone number'/> 
-  
-</div>  
-Enter password
-
-<div>
-
-  <input type='text' className='border border-gray-200 rounded-md ml-2 w-[400px] p-1 focus:border  focus:border-red-400 duration-150 text-gray-500' placeholder='Enter password'/> 
-  
-</div>   
-Re-enter password
-
-<div>
-
-  <input type='text' className='border border-gray-200 rounded-md ml-2 w-[400px] p-1 focus:border  focus:border-red-400 duration-150 text-gray-500' placeholder='Enter password'/> 
-  
-</div>   
-
-            </div>
-            Applicant
-            <input type='radio' />
-        </form>
+            <input type='radio' id='recruiter' name='role' className='mr-2' />
+            <label htmlFor='recruiter'>Recruiter</label>
+          </div>
+        </div>
+        
+        <button 
+          type='button' 
+          className='bg-[#5120a7] text-white p-3 rounded-lg w-full hover:bg-[#3e1785] transition-colors duration-150'
+        >
+          Sign Up
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
