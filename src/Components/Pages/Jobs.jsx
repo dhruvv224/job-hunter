c4c4a63201d7e2f6d0ca787f24dd145a869b976c
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-09-09 14:35:01 +0530

    fixed

--- src/Components/Pages/Jobs.jsx
+++ src/Components/Pages/Jobs.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+// import { useLocation } from 'react-router-dom'
+// const fetchQuery=()=>{
+//     return new URLSearchParams(useLocation.jobs())
+// }
 const Jobs = () => {
+    
+// const Query=fetchQuery()
+// const searchTerm=Query.get('q')
   return (
-    <div>Jobs</div>
+    <div>Jobs
+        {/* {
+            searchTerm
+        } */}
+    </div>
   )
 }
 
