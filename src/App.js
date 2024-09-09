ee9f4d74999d6a7e52e71199006d1ffb8402bb80
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-09-09 14:29:06 +0530

    searchquery

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import { Route,Routes } from 'react-router-dom';
 import SignUpForm from './Components/Pages/SignUpForm';
 import { Login } from './Components/Pages/Login';
 import Home from './Components/Pages/Home';
+import Jobs from './Components/Pages/Jobs';
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
       <Routes>
-      <Route path='/' element={<Home/>}/>
 
+      <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<SignUpForm/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/jobs' element={<Jobs/>}/>
+        
 
 
       </Routes>
