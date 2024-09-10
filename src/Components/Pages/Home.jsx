ad556332bf804eb7359451eabd6addbf64b82acf
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-09-10 16:44:12 +0530

    latestjobs

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Herosection from '../Herosection'
 import CategoryCarousel from '../CategoryCarousel'
+import LatestJobs from '../LatestJobs'
 
 const Home = () => {
   return (
     <div>
       <Herosection/>
       <CategoryCarousel/>
+      <LatestJobs/>
     </div>
   )
 }
