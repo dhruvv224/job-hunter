1357ca43b88b21c305797196b6e2886ad3abccfe
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-09-10 17:07:56 +0530

    commit

--- src/Components/LatestJobs.jsx
+++ src/Components/LatestJobs.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const LatestJobs = () => {
   return (
-    <div>LatestJobs</div>
+    <div>
+        <div className='max-w-7xl  mx-auto my-10'>
+            <h1 className='text-4xl font-bold'>
+                <span className='text-[#5624ac]'> Latest & Top   </span>
+                Job Openings
+            </h1>
+        </div>
+    </div>
   )
 }
 
---
+++ src/Components/LatestJobscard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LatestJobscard = () => {
+  return (
+    <div>LatestJobscard</div>
+  )
+}
+
+export default LatestJobscard
