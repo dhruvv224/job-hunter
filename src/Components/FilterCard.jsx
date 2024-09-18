02e01f265db49ee07ab8b7c983246b432b1fcd5f
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-09-18 20:35:04 +0530

    filter card

--- src/Components/FilterCard.jsx
+++ src/Components/FilterCard.jsx
@@ -1,6 +1,38 @@
 import React from 'react'
 
 const FilterCard = () => {
+  const filterData=[
+    {
+      filterType:"Location",
+      array:["Delhi Ncr","Mumbai","Pune","Banglore",
+        "Ahmedabad"
+      ]
+    },
+    {
+      filterType:"Industry",
+      array:["Software & consultancy","Hr","Developers","Finance",
+        "Sales"
+      ]
+    },
+    {
+      filterType:"Work mode",
+      array:["Hybrid","Work from home","On site",
+      ]
+    },
+    {
+      filterType:"Salary",
+      array:["0-3 lakhs","3-6 lakhs","7-10 lakhs","10-15 lakhs",
+      
+      ]
+    },
+    {
+      filterType:"Top companies",
+      array:["Wipro","Infosys","Microsoft","Amazon",
+        "Flipkart"
+      ]
+    }
+
+  ]
   return (
     <div>FilterCard</div>
   )
